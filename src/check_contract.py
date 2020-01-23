f3e541e4b36ca2c638891853ae2e99056b847c6a
Nancy Schorr <nms@schorrmedia.com>
2020-01-23 00:16:17 -0800

    more changes

--- src/check_contract.py
+++ src/check_contract.py
@@ -16,24 +16,36 @@ class CheckContract(object):
         contract_id = "TTSETeCA3FueL9cKCiDR8vAiRiGVtVCJksEsstM"
         self.start_list = [chain_id, contract_id]
         self.chainId = chain_id
-        self.contract = contract_id
+        self.contract_id = contract_id
         tup_one = ("jsonrpc", "2.0",)
         self.tup_one = tup_one
         self.rand_id = random.randrange(1, 999)
         self.emp_list = []
 
+    # class data_object(object):
+    #     def __init__(self, chain_id, contract_id):
+    #         chain_id
+    #     start_list = [chain_id, contract_id]
+    #     emp_list = []
+    #     req_type = "getAllProductIds"
+    #     p_str = "() return String"
+    #     params_list = [*start_list, req_type, p_str, emp_list]  # 4 items
+    #     params = ("params", params_list,)
 
-    def doit(self, meth_m, params):
-        meth_type = ("method", meth_m,)
+
+    def doit(self, params):
+        method_type = "invokeView"
+        meth_type = ("method", method_type,)
+
         id_tup = ("id", self.rand_id,)
         req = requests.Request('POST', self.url_post, headers=self.myhead)
         req.json = dict([self.tup_one, meth_type, params, id_tup])
-        r = req.prepare()
-        s = requests.Session()
-        rstat = s.send(r)
+        the_req = req.prepare()
+        session = requests.Session()
+        rstat = session.send(the_req)
         print("stat: ", rstat)
-        pastebin_url = rstat.text
-        print("The response is:%s" % pastebin_url)
+        the_resp = rstat.text
+        print("The response is: %s" % the_resp)
         # return rstat
 
     def req_basic(self):
@@ -42,42 +54,40 @@ class CheckContract(object):
         req.json = data
         self.doit(req)
 
-    def req_two(self):
-        method_name = "invokeView"
+    def req_get_all_prod_ids(self):
         req_type = "getAllProductIds"
         p_str = "() return String"
         params_list = [*self.start_list, req_type, p_str, self.emp_list]  # 4 items
         params = ("params", params_list,)
-        self.doit(method_name, params)
+        self.doit(params)
 
 #curl -s -X POST -H -v "Content-Type: application/json"  --data '{"jsonrpc":"2.0",
 # "method":"invokeView","params":[24442,"TTSETeCA3FueL9cKCiDR8vAiRiGVtVCJksEsstM","getReviews","(String productId) return Ljava/util/List;",["baseball"]],"id":914}'  http://78.47.206.255:18003/
 
-    def req_three(self):
-        method_name = "invokeView"
+    def req_get_reviews(self):
         req_type = "getReviews"
         p_str = "(String productId) return Ljava/util/List;"
-        params_list = [*self.start_list, req_type, p_str, ["baseball"]]  # 4 items
+        short_list = ["baseball"]  # 4 items
+        params_list = [*self.start_list, req_type, p_str, short_list]  # 4 items
         params = ("params", params_list,)
-        self.doit(method_name, params)
+        self.doit(params)
 
-     def get_contract(self):
-         method_name = "invokeView"
-         req_type = "getContract"
-         p_str = "(String productId) return Ljava/util/List;"
-         arg0 = ''
-         arg1 = ''
-         short_list = [arg0, arg1]  # 4 items
-         params_list = [*self.start_list, req_type, p_str, short_list]  # 4 items
-         params = ("params", params_list,)
-         self.doit(method_name, params)
+    def req_get_contract(self):
+        req_type = "getContract"
+        p_str = "(String productId) return Ljava/util/List;"
+        arg0 = self.contract_id
+        short_list = [arg0]  # 4 items
+        params_list = [*self.start_list, req_type, p_str, short_list]  # 4 items
+        params = ("params", params_list,)
+        self.doit(params)
 
 
-#          "raw": "{\n\"jsonrpc\":\"2.0\",\n\"method\":\"getContract\",\n\"params\":[arg0, arg1],\n\"id\":1234\n}\n"
+    #   "raw": "{\n\"jsonrpc\":\"2.0\",\n\"method\":\"getContract\",\n\"params\":[arg0,
+    #   arg1],\n\"id\":1234\n}\n"
 
 if __name__ == "__main__":
     c = CheckContract()
-    c.req_three()
+    c.req_get_contract()
 
     print("done")
 
